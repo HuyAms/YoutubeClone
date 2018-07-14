3f17022d1b0cddf371eb71d2d0fda9bca5b8f7a7
HuyAms <dinhhuyams@gmail.com>
2018-07-14 08:57:37 +0300

    Initial Commit

---
+++ YoutubeClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YoutubeClone
+//
+//  Created by iosadmin on 14.7.2018.
+//  Copyright © 2018 iosadmin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
